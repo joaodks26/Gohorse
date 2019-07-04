32939f751b20f1559ca1d2fd5fe178bcfde568fc
joao.pbsousa <jaoobarbosadks26@gmail.com>
2019-07-04 10:49:22 -0300

    Função Fatorial

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         $sub = $numero1 - $numero2;
         echo $sub;      
     }
-    
+
     function multiplicacao($numero1, $numero2)
     {
         $multiplicacao = $numero1 * $numero2;
@@ -19,4 +19,15 @@
         $variavelsoma = $numero1 + $numero2;
         return $variavelsoma;
     }
+
+    function fatorial($numero1)
+    {
+        for ($i = $numero1; $i >= 1; $i--)
+        {
+            $fatorial = $fatorial * $numero1;
+            $numero1 = $numero1 -1;
+        }
+    }
+
+
 
