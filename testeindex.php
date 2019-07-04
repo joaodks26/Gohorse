4e6cad4eabcd82edc2c6f719d6e8b8ec5de36a89
Charles417 <cprovatti@yahoo.com.br>
2019-07-04 11:48:19 -0300

    Execução das funções para teste

---
+++ testeindex.php
@@ -0,0 +1,23 @@
+<?php 
+
+    require("index.php");
+
+    echo "Subtração: ";
+    sub(5, 20);
+    echo "<br>";
+    echo "Soma: ";
+    soma(5, 20);
+    echo "<br>";
+    echo "Multiplicação: ";
+    multiplicacao(5, 10);
+    echo "<br>";
+    echo "Divisão: ";
+    divisao(50, 10);
+    echo "<br>";
+    echo "Fatorial: ";
+    fatorial(7);
+    echo "<br>";
+    echo "Média: ";
+    media(8, 10, 7, 4);
+
+?>
