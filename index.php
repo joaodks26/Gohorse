a8e02d16945b248ad75b833c3ceceb9e6d58bd8f
victor.gbsantos <geovanevictor375@gmail.com>
2019-07-04 11:25:47 -0300

    função de divisão

--- index.php
+++ index.php
@@ -38,3 +38,16 @@
       }
         echo $fatorial;
     }
+    
+    function divisao($numero1, $numero2)
+    {
+        if ($numero2 == 0)
+        {
+            echo " Não dividimos por zero";
+        }        
+        else
+        {
+            $div = $numero1 / $numero2;
+            echo $div;
+        }
+    }
